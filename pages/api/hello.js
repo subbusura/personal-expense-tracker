6887bdb00c3fa62d23443e8bd8ee4be53a5babcf
subbusura <14821273+subbusura@users.noreply.github.com>
2021-02-14 14:58:04 +0530

    --week1 commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,10 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import nc from "next-connect";
 import authMiddleware from "../../middleware/authMiddleware";
-
+import dbMiddleware from "./../../middleware/dbMiddleware";
 export default nc()
+  .use(dbMiddleware)
   .use(authMiddleware)
   .get((req, res) => {
     res.statusCode = 200;
-    res.json({ name: "John Doe this is my final output" });
+    res.json({ name: "" });
   });
